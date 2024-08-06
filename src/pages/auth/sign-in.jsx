ec73067b84cb9e1772916ef02198440d2e3c73f7
julianita112 <juliananbetancur@gmail.com>
2024-08-06 06:33:43 -0500

    juli 6

--- src/pages/auth/sign-in.jsx
+++ src/pages/auth/sign-in.jsx
@@ -1,23 +1,52 @@
+import React, { useState } from "react";
 import {
   Card,
   Input,
   Button,
   Typography,
 } from "@material-tailwind/react";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Swal from "sweetalert2";
 import { useAuth } from "@/context/authContext";
 
+
 export function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
   const navigate = useNavigate();
   const { login, updatePermissions } = useAuth();
 
   const handleSignIn = async (e) => {
     e.preventDefault();
+
+    // Limpiar errores anteriores
+    setEmailError("");
+    setPasswordError("");
+
+    // Validaciones
+    let valid = true;
+    if (!email) {
+      setEmailError("El campo de correo electrónico es obligatorio.");
+      valid = false;
+    } else if (email.length < 4 || !email.includes("@")) {
+      setEmailError("Ingrese un correo electrónico válido.");
+      valid = false;
+    }
+
+    if (!password) {
+      setPasswordError("El campo de contraseña es obligatorio.");
+      valid = false;
+    } else if (password.length < 4) {
+      setPasswordError("La contraseña debe tener al menos 4 caracteres.");
+      valid = false;
+    }
+
+    if (!valid) {
+      return;
+    }
 
     const Toast = Swal.mixin({
       toast: true,
@@ -46,7 +75,6 @@ export function SignIn() {
 
         localStorage.setItem("token", token);
 
-        // Obtener la información del usuario autenticado
         const userResponse = await axios.get("http://localhost:3000/api/usuarios", {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -63,7 +91,6 @@ export function SignIn() {
 
         console.log("User data:", user);
 
-        // Obtener el rol del usuario y los permisos asociados
         try {
           const roleResponse = await axios.get(`http://localhost:3000/api/roles/${user.id_rol}`, {
             headers: {
@@ -85,7 +112,7 @@ export function SignIn() {
 
           Toast.fire({
             icon: "success",
-            title: "Acceso concedido"
+            title: "Acceso concedido."
           });
 
           navigate("/dashboard/home");
@@ -109,58 +136,64 @@ export function SignIn() {
   };
 
   return (
-    <section className="m-7 flex gap-4 bg-gradient-to-br from-white to-white">
-      <div className="w-full lg:w-1/2 mt-">
-        <Card className="p-12 shadow-xl rounded-lg bg-white">
+    <section className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-white py-12 px-4">
+      <div className="flex flex-col lg:flex-row w-full max-w-4xl mx-auto">
+        <div className="lg:w-1/2 p-6 bg-white rounded-lg shadow-lg">
           <div className="text-center mb-6">
-            <img src="/img/delicremlogo.png" alt="Logo" className="mx-auto mb-1" style={{ width: '250px' }} />
-            <Typography variant="h3" className="text-black font-bold mb-4">Iniciar Sesión</Typography>
-            <Typography variant="paragraph" color="blue-gray" className="text-lg font-normal">Ingrese su correo electrónico y contraseña para iniciar sesión.</Typography>
+            <img src="/img/delicremlogo.png" alt="Logo" className="mx-auto mb-4" style={{ width: '250px' }} />
+            <Typography variant="h4" className="text-gray-800 font-semibold mb-2">Iniciar Sesión</Typography>
+            <Typography variant="body1" className="text-gray-600">Ingrese su correo electrónico y contraseña para iniciar sesión.</Typography>
           </div>
-          <form className="mt-8" onSubmit={handleSignIn}>
-            <div className="mb-4">
-              <Typography variant="small" color="blue-gray" className="mb-2 block font-medium">
-                Email
-              </Typography>
-              <Input
-                size="lg"
-                placeholder="usuario@gmail.com"
-                className="w-full border-t-blue-gray-200 focus:border-t-gray-900"
-                labelProps={{
-                  className: "before:content-none after:content-none",
-                }}
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="mb-4">
-              <Typography variant="small" color="blue-gray" className="mb-2 block font-medium">
-                Contraseña
-              </Typography>
-              <Input
-                type="password"
-                size="lg"
-                placeholder="********"
-                className="w-full border-t-blue-gray-200 focus:border-t-gray-900"
-                labelProps={{
-                  className: "before:content-none after:content-none",
-                }}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <Button type="submit" className="mt-6" fullWidth>
-              Iniciar Sesión
-            </Button>
-          </form>
-        </Card>
-      </div>
-      <div className="w-1/2 h-full hidden lg:block shadow-xl">
-        <img
-          src="/img/imalogin.jpeg"
-          className="h-full w-full object-cover rounded-lg"
-          alt="Background"
-        />
+          <Card className="shadow-none p-4">
+            <form className="space-y-4" onSubmit={handleSignIn}>
+              <div>
+                <Typography variant="small" color="blue-gray" className="block font-medium mb-1">
+                  Email
+                </Typography>
+                <Input
+                  size="md"
+                  placeholder="usuario@gmail.com"
+                  className="w-full border-gray-300 rounded-lg focus:border-pink-200 focus:ring-1"
+                  labelProps={{
+                    className: "before:content-none after:content-none",
+                  }}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  error={!!emailError}
+                />
+                <Typography className={`error-message ${emailError ? 'show' : ''}`}>{emailError}</Typography>
+              </div>
+              <div>
+                <Typography variant="small" color="blue-gray" className="block font-medium mb-1">
+                  Contraseña
+                </Typography>
+                <Input
+                  type="password"
+                  size="md"
+                  placeholder="********"
+                  className="w-full border-gray-300 rounded-lg focus:border-pink-200 focus:ring-1"
+                  labelProps={{
+                    className: "before:content-none after:content-none",
+                  }}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  error={!!passwordError}
+                />
+                <Typography className={`error-message ${passwordError ? 'show' : ''}`}>{passwordError}</Typography>
+              </div>
+              <Button type="submit" className="w-full bg-black hover:bg-pink-700 text-white py-2 px-4 rounded-lg transition duration-300">
+                Iniciar Sesión
+              </Button>
+            </form>
+          </Card>
+        </div>
+        <div className="lg:w-1/2 hidden lg:block bg-gradient-to-br from-blue-100 to-blue-300 rounded-lg overflow-hidden">
+          <img
+            src="/img/imalogin.jpeg"
+            className="h-full w-full object-cover rounded-lg"
+            alt="Background"
+          />
+        </div>
       </div>
     </section>
   );
